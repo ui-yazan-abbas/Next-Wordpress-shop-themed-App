742fc3256bc1d71a7df5b00a6b21ac84212ea3fd
Yazan Abbas <yazan.abbas@ui.com>
2024-02-05 14:39:58 +0100

    Restructure App Folders

---
+++ src/app/[lang]/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Cart
+    </div>
+  );
+};
+
+export default Cart;
