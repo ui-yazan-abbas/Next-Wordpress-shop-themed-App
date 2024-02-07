7213ce6efbaeb1d42498cec0d5486e748d69869e
Yazan Abbas <yazan.abbas@ui.com>
2024-02-07 16:09:16 +0100

    Add Header responsivness

--- src/app/[lang]/components/CustomLi.tsx
+++ src/app/[lang]/components/CustomLi.tsx
@@ -7,7 +7,7 @@ const CustomLi: FC<
 > = ({ isShopPage = false, children }) => {
   return (
     <li
-      className={`relative text-base ${
+      className={`relative text-base sm:text-lg md:text-xl ${
         isShopPage ? "after:bg-black" : "after:bg-white"
       } after:absolute after:h-px after:w-0 after:top-5 after:left-0 hover:after:w-full after:transition-all after:duration-300`}
     >
